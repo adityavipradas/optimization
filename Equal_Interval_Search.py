d900040771156c165b10afdb62f22514b4062ecc
Aditya Vipradas <adityavipradas@yahoo.com>
2013-01-18 12:52:35 -0800

    Two Point Equal Interval Search Method
    This code is written in Python 2.7.3 on Ubuntu 12.04 LTS using spyder IDE. The code depicts a one-dimensional optimization technique named as "Two Point Equal Interval Search Method" for locating a local minima in a continuous and unimodal function within a specified closed interval. Any desired function can be specified within the user-defined function 'equation()' in the code. This code requires upper and lower limits of the closed interval along with the final interval of uncertainty in percentage(error) as inputs. Minima, value of the function at minima, number of iterations required and a plot of the specified function within the specified closed interval(for verification) are given as the outputs.

---
+++ Equal_Interval_Search.py
@@ -0,0 +1,68 @@
+"""
+Created on Sat Jan 19 01:20:02 2013
+
+@author: adityavipradas
+"""
+"""Two point equal interval search method for finding the local minima within a specified closed interval
+Function within the interval being continous and unimodal"""
+
+"""import libraries"""
+import math
+from pylab import*
+
+"""define function"""
+"""change the function as per requirement"""
+def equation(x):
+    y = (x-1)*(x-2)*(x-3)
+    return y
+
+"""accept inputs"""  
+a = float(raw_input("Enter the lower limit of the closed interval: "))
+b = float(raw_input("Enter the upper limit of the closed interval: "))
+e = float(raw_input("Enter the final interval of uncertainty(%): "))
+ak = a
+bk = b
+iter = 0
+
+"""solve"""
+while (math.fabs(b - a)>(e/100.)):
+    
+    """increment the iteration count"""
+    iter = iter + 1
+    
+    """calculate the values of new points x1 and x2"""
+    x1 = a + ((b - a)/3.)
+    x2 = b - ((b - a)/3.)
+    
+    """evaluate the values of function at x1 and x2"""
+    f1 = equation(x1)
+    f2 = equation(x2)
+    
+    """eliminate the undesired region"""
+    if (f1 < f2):
+        b = x2
+    elif (f2 < f1):
+        a = x1
+    else:
+        b = x2
+        a = x1
+        
+"""store the minima value in xstar"""
+if (f1 < f2):
+    xstar = x1
+else:
+    xstar = x2
+
+"""plot"""    
+x = []
+val = []
+for i in frange(ak, bk, (e/100.)):
+    x.append(i)
+    val.append(equation(i))
+print "Minimum value is at x = ", xstar,"\n", "Function value is y = ",equation(xstar)
+print "Number of iterations = ",iter
+plot(x, val)
+xlabel("x")
+ylabel("f(x)")
+grid()
+show()
