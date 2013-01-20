cb7efc0286f4700de2ebff96445d0576ccfc5574
Aditya Vipradas <adityavipradas@yahoo.com>
2013-01-20 08:35:11 -0800

    Golden Section Search
    This code is written in Python 2.7.3 using spyder IDE on Ubuntu 12.04 LTS. This code employs the Golden Section Search for locating a local minima in a function specified by the user in the user-defined 'equation()' function in the required closed interval. End points of the closed interval and final interval of uncertainty are required as inputs. Output is generated in the form of number of iterations required and the x and f(x) values corresponding to the local minima. This code also generates a table showing the interval end points after every iteration along with a plot of the function for verification purposes.

---
+++ Golden_Search.py
@@ -0,0 +1,81 @@
+"""
+Created on Sun Jan 20 20:44:47 2013
+
+@author: adityavipradas
+"""
+
+"""Golden Section Search for locating a local minima of a function 
+in a specified closed interval"""
+"""Function is continuous and unimodal"""
+
+"""import libraries"""
+import math
+from pylab import*
+
+"""define function"""
+"""change the function as per requirement"""
+def equation(x):
+    y = (2*math.sin(x))-((x**2)/10.)
+    return y
+    
+"""accept inputs"""  
+a = float(raw_input("Enter the lower limit of the closed interval: "))
+b = float(raw_input("Enter the upper limit of the closed interval: "))
+e = float(raw_input("Enter the final interval of uncertainty(%): "))
+ak = a
+bk = b
+iter = 1
+
+"""initialize the ratio"""
+r = (3 - math.sqrt(5))/2.
+
+"""solve"""
+x1 = a + r*(b - a)
+x2 = b - r*(b - a)
+f1 = equation(x1)
+f2 = equation(x2)
+print"\n"
+print "iteration \t a \t\t b"
+print 1,"\t",a,"\t\t",b
+
+while(math.fabs(b - a)>(e/100.)):
+    
+    """increment the number of iterations"""
+    iter = iter + 1
+    
+    """eliminate the undesired region"""
+    if (f1 <= f2):
+        b = x2
+        x2 = x1
+        x1 = a + r*(b - a)
+        f2 = f1
+        f1 = equation(x1)
+        print iter,"\t",a,"\t",b
+    else:
+        a = x1
+        x1 = x2
+        x2 = b - r*(b - a)
+        f1 = f2
+        f2 = equation(x2)
+        print iter,"\t",a,"\t",b
+        
+"""store the minima value in xstar"""
+if (f1 <= f2):
+    xstar = x1
+else:
+    xstar = x2
+
+"""plot"""    
+x = []
+val = []
+for i in frange(ak, bk, 0.01):
+    x.append(i)
+    val.append(equation(i))
+print "\n"
+print "Minimum value is at x = ", xstar,"\n", "Function value at x is y = ",equation(xstar)
+print "Number of iterations = ",iter
+plot(x, val)
+xlabel("x")
+ylabel("f(x)")
+grid()
+show()
