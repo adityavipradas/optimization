2121ce46668b15e7a017659d41718084c1f418c4
Aditya Vipradas <adityavipradas@yahoo.com>
2013-01-20 02:44:24 -0800

    Bisection Method
    This code is written in Python 2.7.3 on Ubuntu 12.04 LTS using spyder IDE. The code depicts a one-dimensional optimization technique named as "Bisection Method" for locating a local minima in a continuous and unimodal function within a specified closed interval. Any desired function can be specified within the user-defined function 'equation()' in the code. This code requires upper and lower limits of the closed interval along with the final interval of uncertainty in percentage(error) as inputs. Minima, value of the function at minima, number of iterations required and a plot of the specified function within the specified closed interval(for verification) are given as the output.

---
+++ Bisection_Method.py
@@ -0,0 +1,66 @@
+"""
+Created on Sun Jan 20 00:43:19 2013
+
+@author: adityavipradas
+"""
+
+"""Bisection Method for locating a local minima of a function in a specified closed interval
+Function in the closed interval is continuous and unimodal"""
+
+"""import libraries"""
+import math
+from pylab import*
+
+"""define function"""
+"""change the function as per requirement"""
+def equation(x):
+    y = (x-1)*(x-2)*(x-3)
+    return y
+
+"""accept inputs"""  
+a = float(raw_input("Enter the lower limit of the closed interval: "))
+b = float(raw_input("Enter the upper limit of the closed interval: "))
+e = float(raw_input("Enter the final interval of uncertainty(%): "))
+ak = a
+bk = b
+iter = 0
+
+"""solve"""
+while (math.fabs(b - a)>(e/100.)):
+    
+    """increment the iteration count"""
+    iter = iter + 1
+
+    """calculate the values of new points x1 and x2"""
+    x1 = ((a + b)/2.) - 0.000000372
+    x2 = ((a + b)/2.) + 0.000000372
+
+    """evaluate the values of function at x1 and x2"""
+    f1 = equation(x1)
+    f2 = equation(x2)
+    
+    """eliminate the undesired region"""
+    if (f1 < f2):
+        b = x2
+    elif (f2 < f1):
+        a = x1
+
+"""store the minima value in xstar"""
+if (f1 < f2):
+    xstar = x1
+else:
+    xstar = x2
+
+"""plot"""    
+x = []
+val = []
+for i in frange(ak, bk, 0.01):
+    x.append(i)
+    val.append(equation(i))
+print "Minimum value is at x = ", xstar,"\n", "Function value is y = ",equation(xstar)
+print "Number of iterations = ",iter
+plot(x, val)
+xlabel("x")
+ylabel("f(x)")
+grid()
+show()
